67993b38ab87444945403465f39f8dfb4c442125
wachira <wachirastudeo@gmail.com>
2024-12-28 10:40:52 +0700

    update views regsiter and logout

--- api/views.py
+++ api/views.py
@@ -2,9 +2,10 @@ from rest_framework import viewsets, generics, permissions
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
 from .models import Intern, Education, Training, WorkExperience
-from .serializers import InternSerializer, EducationSerializer, TrainingSerializer, WorkExperienceSerializer
+from .serializers import InternSerializer, EducationSerializer, TrainingSerializer, WorkExperienceSerializer,RegisterSerializer
 
 # Create your views here.
 class InternViewSet(viewsets.ModelViewSet):
@@ -22,4 +23,24 @@ class TrainingViewSet(viewsets.ModelViewSet):
 class WorkExperienceViewSet(viewsets.ModelViewSet):
     queryset = WorkExperience.objects.all()
     serializer_class = WorkExperienceSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class LogoutView(APIView):
+    swagger_fake_view = True
+
+    def post(self, request,**args, **kwargs):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 

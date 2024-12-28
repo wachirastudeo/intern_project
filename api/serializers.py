af3197f8624c8fa3ae8edfc0d612306b03717b3d
wachira <wachirastudeo@gmail.com>
2024-12-28 10:36:54 +0700

    serializer add logout register

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from .models import Intern, Education, Training, WorkExperience
 
@@ -23,6 +24,49 @@ class WorkExperienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = WorkExperience
         fields = '__all__'
+class RegisterSerializer(serializers.ModelSerializer):
+    tel = serializers.CharField(max_length=15, required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'first_name', 'last_name', 'email', 'tel')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user= User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+        )
+        user.profile.tel = validated_data['tel']
+        user.profile.save()
+        return user
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()  # รับค่า refresh token
+
+
+class LogoutView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = LogoutSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        if getattr(self, 'swagger_fake_view', False):  # ตรวจสอบว่าเป็น Swagger หรือไม่
+            return None
+        return super().get_serializer(*args, **kwargs)
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            refresh_token = serializer.validated_data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=204)
+        except Exception:
+            return Response(status=400)
 
  
 
